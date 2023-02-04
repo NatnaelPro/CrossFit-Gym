3d2f5ed8ccc31b0bc6b15cbfb6476e6402a67bcb
Natnael <90891903+NatnaelPro@users.noreply.github.com>
2023-02-04 23:09:52 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const hamburger = document.querySelector('.hamburger');
+        const nav = document.querySelector('nav');
+        const container = document.querySelector('#hero .container');
+        const aboutHero = document.querySelector('.about-header');
+        const contactHero = document.querySelector('#contact-hero .container');
